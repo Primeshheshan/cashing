085156e2f3354f5c649ac146973f391bd401d304
PrimeshHeshan <primeshheshan@gmail.com>
2023-09-07 19:08:01 +0530

    feat: redis for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { CachingModule } from './caching/caching.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   // setting isGlobal: true will make the cache available to all modules in the application
-  imports: [CacheModule.register({ isGlobal: true }), CachingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME, // new property
+      password: process.env.REDIS_PASSWORD, // new property
+      no_ready_check: true, // new property
+    }),
+    CachingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
